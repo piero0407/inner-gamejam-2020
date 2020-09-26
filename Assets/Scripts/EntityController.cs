c68cfe0163e6edec22fd9c8e6c668cc3aa70575b
Piero0407 <piero.barbieri50@gmail.com>
2020-09-26 14:48:19 -0500

    movement, controls and collisions

---
+++ Assets/Scripts/EntityController.cs
@@ -0,0 +1,69 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EntityController : MonoBehaviour
+{
+    //Inspector Properties
+    [FoldoutGroup("References")]
+    [SerializeField] new Rigidbody rigidbody;
+
+    [TabGroup("TabGroup1", "Stats")]
+    [HideLabel]
+    [SerializeField] EntityStats stats;
+
+    [TabGroup("TabGroup1","Collision")]
+    [SerializeField] LayerMask damageLayers;
+
+    //Logic Variables
+    float currentLives;
+    Vector2 m_Move;
+
+    private void Awake()
+    {
+        currentLives = stats.MaxLives;
+    }
+
+    private void OnValidate()
+    {
+        if(stats != null && rigidbody != null)
+            rigidbody.drag = stats.Drag;
+    }
+
+    private void FixedUpdate()
+    {
+        //Rotation: m_Move.x [-1...0...1]
+        rigidbody.rotation *= Quaternion.Euler(0, 0, -m_Move.x * stats.AngularVelocity * Time.fixedDeltaTime);
+
+        if (m_Move.y > 0f)
+        {
+            rigidbody.velocity += transform.up * m_Move.y * stats.Acceleration * Time.fixedDeltaTime; //Basic Movement
+        }
+        else
+        {
+            //TODO: "Dash" or Slingshot logic
+        }
+
+        //Limit rigidbody.velocity to MaxSpeed
+        if (rigidbody.velocity.magnitude > stats.MaxSpeed)
+        {
+            rigidbody.velocity = rigidbody.velocity.normalized * stats.MaxSpeed;
+        }
+    }
+
+    public void OnMove(InputValue value)
+    {
+        m_Move = value.Get<Vector2>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(((1 << collision.gameObject.layer) & damageLayers.value) > 0)
+        {
+            //TODO: Collision Logic
+            print("I collided with something!!!");
+        }
+    }
+}
---
+++ Assets/Scripts/EntityStats.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[InlineEditor(Expanded = true)]
+[CreateAssetMenu(fileName = "EntityData", menuName = "ScriptableObjects/EntityData")]
+public class EntityStats : ScriptableObject
+{
+    [BoxGroup("Base Stats")]
+    [SerializeField] int maxLives = 2;
+    [BoxGroup("Rotation Stats"), Range(100, 500)]
+    [SerializeField] float angularVelocity;
+    [BoxGroup("Movement Stats"), Range(1, 200)]
+    [SerializeField] float acceleration;
+    [BoxGroup("Movement Stats"), Range(1, 10)]
+    [SerializeField] float maxSpeed;
+    [BoxGroup("Movement Stats"), Range(1, 10)]
+    [SerializeField] float drag = 5;
+
+    public int MaxLives { get => maxLives; }
+    public float AngularVelocity { get => angularVelocity; }
+    public float Acceleration { get => acceleration; }
+    public float MaxSpeed { get => maxSpeed; }
+    public float Drag { get => drag; }
+}
